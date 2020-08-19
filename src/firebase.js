8677513069737272058cf54ed1d12b99b899fc98
andrencam <naka.campos05@gmail.com>
2020-08-19 00:25:11 -0700

    new Whatsapp project files

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAvK_BYs6u4FVwdBKmACxtaMxeBqhCnI9U",
+  authDomain: "whatsapp-41b79.firebaseapp.com",
+  databaseURL: "https://whatsapp-41b79.firebaseio.com",
+  projectId: "whatsapp-41b79",
+  storageBucket: "whatsapp-41b79.appspot.com",
+  messagingSenderId: "770487733011",
+  appId: "1:770487733011:web:7c1f9c5faa862d94ed28e6",
+  measurementId: "G-C9TTNCREBZ",
+};
